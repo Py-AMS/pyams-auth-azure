77c0628a15c7e7c9263be4059e77695bc708ffc0
Thierry Florac <tflorac@ulthar.net>
2023-11-26 01:41:17 +0100

    Version 2.0.0

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@ from setuptools import setup, find_packages
 DOCS = os.path.join(os.path.dirname(__file__),
                     'docs')
 
-README = os.path.join(DOCS, 'README.txt')
-HISTORY = os.path.join(DOCS, 'HISTORY.txt')
+README = os.path.join(DOCS, 'README.rst')
+HISTORY = os.path.join(DOCS, 'HISTORY.rst')
 
-version = '1.1.3'
+version = '2.0.0'
 long_description = open(README).read() + '\n\n' + open(HISTORY).read()
 
 tests_require = [
@@ -69,7 +69,7 @@ setup(name='pyams_auth_azure',
           'persistent',
           'pyams_security',
           'pyams_utils',
-          'pyramid',
+          'pyramid >= 2.0.0',
           'zope.container',
           'zope.interface',
           'zope.schema'
